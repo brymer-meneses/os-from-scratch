0843c73105084e662301b96a7def0f0d64d3faec
Brymer Meneses <68867487+brymer-meneses@users.noreply.github.com>
2023-06-26 02:50:25 +0800

    improve makefile and clear screen

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,15 +1,23 @@
+
 #define VIDEO_ADDRESS 0xb8000
 #define MAX_ROWS 25
 #define MAX_COLS 80
 
-#define REG_SCREEN_CTRL 0x3D4
-#define REG_SCREEN_DATA 0x3D5
-
 #define WHITE_ON_BLACK 0x0f
 
+void clear_screen() {
+    // Get a pointer to the video memory
+    char* video_memory = (char*)0xB8000;
+    
+    // Clear each character cell
+    for (int i = 0; i < MAX_ROWS * MAX_COLS * 2; i += 2) {
+        video_memory[i] = ' ';
+        video_memory[i + 1] = WHITE_ON_BLACK;  // Attribute byte for white-on-black
+    }
+}
+
 int main() {
-  char* video_memory = (char*) 0xb8000;
-  *video_memory = 'X';
+  clear_screen();
 
   return 0;
 }
